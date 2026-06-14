4e30c75987314422e1953ca390587f38e5672430
DwitiChoksi <114918911+DwitiChoksi@users.noreply.github.com>
2026-06-14 12:16:29 -0400

    Refactor API call for watermelon inside ingredient loop
    Moved the API request for watermelon to within the loop that processes ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ if ingredients_list :
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -43,7 +45,4 @@ if ingredients_list :
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response)
-st_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
